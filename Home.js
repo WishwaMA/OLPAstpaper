8061db3e9b09c7bab06d31254b378be8b65f1e58
WishwaMA <154093373+WishwaMA@users.noreply.github.com>
2024-06-06 18:48:24 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,42 @@
+document.getElementById("sinhala").onclick = function() {
+    window.location.href = "Sinhala.html";
+};
+
+document.getElementById("maths").onclick = function() {
+    window.location.href = "Maths.html";
+}
+
+document.getElementById("science").onclick = function() {
+    window.location.href = "science.html";
+}
+
+document.getElementById("english").onclick = function() {
+    window.location.href = "english.html";
+}
+
+document.getElementById("history").onclick = function () {
+    window.location.href = "History.html";
+}
+document.getElementById("Buddhist").onclick = function () {
+    window.location.href = "buddhist.html";
+}
+
+document.getElementById("tamil").onclick = function() {
+    window.location.href = "tamil.html";
+}
+
+document.getElementById("Acc").onclick = function() {
+    window.location.href = "Acc.html";
+}
+
+document.getElementById("geo").onclick = function() {
+    window.location.href = "geo.html";
+}
+
+document.getElementById("civic").onclick = function() {
+    window.location.href = "civic.html";
+}
+
+document.getElementById("it").onclick = function() {
+    window.location.href = "ICT.html";
+}
